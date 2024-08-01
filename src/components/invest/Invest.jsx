b5939cd39b8d69c60f1951b7cf8ae12625fe19d0
Shoaib-khan644 <mshoaibkhan2580@gmail.com>
2024-08-02 03:24:11 +0500

    anmition2

--- src/components/invest/Invest.jsx
+++ src/components/invest/Invest.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { motion } from "framer-motion";
 import map from "../../assets/icons/map.png";
-import line1 from "../../assets/icons/line-1.png";
-import line2 from "../../assets/icons/line-2.png";
 import investLeft from "../../assets/icons/invest-left.png";
 import investRight from "../../assets/icons/invest-right.png";
 import invetMobileIcon from "../../assets/icons/investMobileIcon.png";
+import './Invest.css'; // Import the CSS file
+
+// Define animation variants
+const mapVariants = {
+  initial: { opacity: 0, scale: 0.8 },
+  animate: { opacity: 1, scale: 1 },
+  exit: { opacity: 0, scale: 0.8 }
+};
 
 function Invest() {
   return (
@@ -16,25 +23,59 @@ function Invest() {
 
       <div className="block w-full lg:flex justify-center items-center lg:px-[5rem] lg:mt-[4rem]">
         <div>
-          <img
+          <motion.img
             className="w-full max-w-[350px] h-[300px] hidden xl:block lg:block md:hidden"
             src={investLeft}
-            alt=""
+            alt="Invest Left"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.8, ease: 'easeInOut' }}
           />
-          <img
+          <motion.img
             className="w-full max-w-[400px] block xl:hidden lg:hidden md:block mx-auto"
             src={invetMobileIcon}
-            alt=""
+            alt="Invest Mobile Icon"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.8, ease: 'easeInOut' }}
           />
         </div>
-        <div>
-          <img className="w-[500px] mx-auto" src={map} alt="" />
+        <div className="relative">
+          <motion.img
+            className="w-[500px] mx-auto"
+            src={map}
+            alt="Map"
+            variants={mapVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{ duration: 1.5, ease: 'easeInOut' }}
+          />
+          <div className="cross-overlay">
+            <div className="cross">
+              <motion.div
+                className="line diagonal1"
+                initial={{ width: 0 }}
+                animate={{ width: ["0%", "100%", "0%"] }}
+                transition={{ duration: 4, repeat: Infinity, repeatType: 'loop', ease: 'easeInOut' }}
+              />
+              <motion.div
+                className="line diagonal2"
+                initial={{ width: 0 }}
+                animate={{ width: ["0%", "100%", "0%"] }}
+                transition={{ duration: 4, repeat: Infinity, repeatType: 'loop', ease: 'easeInOut' }}
+              />
+            </div>
+          </div>
         </div>
         <div>
-          <img
+          <motion.img
             className="w-full max-w-[350px] h-[300px] hidden xl:block lg:block md:hidden"
             src={investRight}
-            alt=""
+            alt="Invest Right"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.8, ease: 'easeInOut' }}
           />
         </div>
       </div>
