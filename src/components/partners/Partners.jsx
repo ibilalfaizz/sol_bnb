f9edd14d37ebdd8c4f91e5ea7cc4c204e7ed3da0
Shoaib-khan644 <mshoaibkhan2580@gmail.com>
2024-08-08 04:27:09 +0500

    adjust32

--- src/components/partners/Partners.jsx
+++ src/components/partners/Partners.jsx
@@ -16,6 +16,22 @@ import partner13 from "../../assets/icons/partner13.png";
 import partner14 from "../../assets/icons/partner14.png";
 import partner15 from "../../assets/icons/partner15.png";
 
+import partner1Hover from "../../assets/icons/partner1-hover.png";
+import partner2Hover from "../../assets/icons/partner2-hover.png";
+import partner3Hover from "../../assets/icons/partner3-hover.png";
+import partner4Hover from "../../assets/icons/partner4-hover.png";
+import partner5Hover from "../../assets/icons/partner5-hover.png";
+import partner6Hover from "../../assets/icons/partner6-hover.png";
+import partner7Hover from "../../assets/icons/partner7-hover.png";
+import partner8Hover from "../../assets/icons/partner8-hover.png";
+import partner9Hover from "../../assets/icons/partner9-hover.png";
+import partner10Hover from "../../assets/icons/partner10-hover.png";
+import partner11Hover from "../../assets/icons/partner11-hover.png";
+import partner12Hover from "../../assets/icons/partner12-hover.png";
+import partner13Hover from "../../assets/icons/partner13-hover.png";
+import partner14Hover from "../../assets/icons/partner14-hover.png";
+import partner15Hover from "../../assets/icons/partner15-hover.png";
+
 const containerVariants = {
   hidden: {
     opacity: 0,
@@ -23,7 +39,7 @@ const containerVariants = {
   visible: {
     opacity: 1,
     transition: {
-      staggerChildren: 0.2, // Adjusts delay between animations of each child
+      staggerChildren: 0.2,
       duration: 0.5,
     },
   },
@@ -32,7 +48,7 @@ const containerVariants = {
 const itemVariants = {
   hidden: {
     opacity: 0,
-    x: 100, // Start from right
+    x: 100,
   },
   visible: {
     opacity: 1,
@@ -46,6 +62,7 @@ const itemVariants = {
 
 function Partners() {
   const [isVisible, setIsVisible] = useState(false);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -58,12 +75,30 @@ function Partners() {
     };
 
     window.addEventListener('scroll', handleScroll);
-    handleScroll(); // Check on mount
+    handleScroll();
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+
+  const partners = [
+    { default: partner1, hover: partner1Hover },
+    { default: partner2, hover: partner2Hover },
+    { default: partner3, hover: partner3Hover },
+    { default: partner4, hover: partner4Hover },
+    { default: partner5, hover: partner5Hover },
+    { default: partner6, hover: partner6Hover },
+    { default: partner7, hover: partner7Hover },
+    { default: partner8, hover: partner8Hover },
+    { default: partner9, hover: partner9Hover },
+    { default: partner10, hover: partner10Hover },
+    { default: partner11, hover: partner11Hover },
+    { default: partner12, hover: partner12Hover },
+    { default: partner13, hover: partner13Hover },
+    { default: partner14, hover: partner14Hover },
+    { default: partner15, hover: partner15Hover },
+  ];
 
   return (
     <div className="lg:mx-[8rem] mt-[2rem] lg:mt-[5rem] partners-container">
@@ -77,13 +112,19 @@ function Partners() {
         initial="hidden"
         animate={isVisible ? "visible" : "hidden"}
       >
-        {[partner1, partner2, partner3, partner4, partner5, partner6, partner7, partner8, partner9, partner10, partner11, partner12, partner13, partner14, partner15].map((partner, index) => (
+        {partners.map((partner, index) => (
           <motion.div
             key={index}
             variants={itemVariants}
             className='item'
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
           >
-            <img className='w-[180px]' src={partner} alt='' />
+            <img
+              className='w-[180px]'
+              src={hoveredIndex === index ? partner.hover : partner.default}
+              alt={`Partner ${index + 1}`}
+            />
           </motion.div>
         ))}
       </motion.div>
